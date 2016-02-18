3e5d026a8feef239c3bf70625251d150d75bef9b
DuyHai DOAN <doanduyhai@gmail.com>
2016-02-18 21:12:31 +0100

    Make SELECT DSL also return TypedMap in addition of entity

--- achilles-core/src/main/java/info/archinnov/achilles/internals/query/dsl/select/AbstractSelectWhere.java
+++ achilles-core/src/main/java/info/archinnov/achilles/internals/query/dsl/select/AbstractSelectWhere.java
@@ -22,6 +22,7 @@ import static java.util.stream.Collectors.toList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,23 +32,27 @@ import com.datastax.driver.core.ExecutionInfo;
 import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.querybuilder.Select;
+import com.google.common.util.concurrent.Uninterruptibles;
 
 import info.archinnov.achilles.internals.metamodel.AbstractEntityProperty;
 import info.archinnov.achilles.internals.options.Options;
 import info.archinnov.achilles.internals.query.StatementProvider;
 import info.archinnov.achilles.internals.query.action.SelectAction;
 import info.archinnov.achilles.internals.query.options.AbstractOptionsForSelect;
+import info.archinnov.achilles.internals.query.raw.TypedMapAware;
 import info.archinnov.achilles.internals.runtime.RuntimeEngine;
 import info.archinnov.achilles.internals.statements.BoundStatementWrapper;
 import info.archinnov.achilles.internals.statements.OperationType;
 import info.archinnov.achilles.internals.statements.StatementWrapper;
 import info.archinnov.achilles.internals.types.EntityIteratorWrapper;
+import info.archinnov.achilles.internals.types.TypedMapIteratorWrapper;
+import info.archinnov.achilles.type.TypedMap;
 import info.archinnov.achilles.type.interceptor.Event;
 import info.archinnov.achilles.type.tuples.Tuple2;
 
 public abstract class AbstractSelectWhere<T extends AbstractSelectWhere<T, ENTITY>, ENTITY>
         extends AbstractOptionsForSelect<T>
-        implements SelectAction<ENTITY>, StatementProvider {
+        implements SelectAction<ENTITY>, StatementProvider, TypedMapAware {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractSelectWhere.class);
 
@@ -83,6 +88,21 @@ public abstract class AbstractSelectWhere<T extends AbstractSelectWhere<T, ENTIT
 
         CompletableFuture<ResultSet> futureRS = rte.execute(statementWrapper);
         return new EntityIteratorWrapper<>(futureRS, meta, statementWrapper, options);
+    }
+
+    public Iterator<TypedMap> typedMapIterator() {
+        final RuntimeEngine rte = getRte();
+        final Options options = getOptions();
+        final StatementWrapper statementWrapper = getInternalBoundStatementWrapper();
+
+        if (LOGGER.isTraceEnabled()) {
+            LOGGER.trace(format("Execute native query async with execution info : %s",
+                    statementWrapper.getBoundStatement().preparedStatement().getQueryString()));
+        }
+
+        CompletableFuture<ResultSet> futureRS = rte.execute(statementWrapper);
+
+        return new TypedMapIteratorWrapper(futureRS, statementWrapper, options);
     }
 
     public CompletableFuture<Tuple2<List<ENTITY>, ExecutionInfo>> getListAsyncWithStats() {
@@ -121,6 +141,93 @@ public abstract class AbstractSelectWhere<T extends AbstractSelectWhere<T, ENTIT
                 });
     }
 
+    /***************************************************************************************
+     * TypedMap API                                                                        *
+     ***************************************************************************************/
+    public CompletableFuture<Tuple2<List<TypedMap>, ExecutionInfo>> getTypeMapsAsyncWithStats() {
+        final RuntimeEngine rte = getRte();
+        final Options options = getOptions();
+
+        final StatementWrapper statementWrapper = getInternalBoundStatementWrapper();
+
+        if (LOGGER.isTraceEnabled()) {
+            LOGGER.trace(format("Select async with execution info : %s",
+                    statementWrapper.getBoundStatement().preparedStatement().getQueryString()));
+        }
+        CompletableFuture<ResultSet> futureRS = rte.execute(statementWrapper);
+
+        return futureRS
+            .thenApply(options::resultSetAsyncListener)
+                    .thenApply(statementWrapper::logReturnResults)
+                    .thenApply(statementWrapper::logTrace)
+                    .thenApply(x -> Tuple2.of(mapResultSetToTypedMaps(x), x.getExecutionInfo()));
+    }
+
+    public CompletableFuture<List<TypedMap>> getTypeMapsAsync() {
+        return getTypeMapsAsyncWithStats()
+                .thenApply(Tuple2::_1);
+    }
+
+    public Tuple2<List<TypedMap>, ExecutionInfo> getTypeMapsWithStats() {
+        try {
+            return Uninterruptibles.getUninterruptibly(getTypeMapsAsyncWithStats());
+        } catch (ExecutionException e) {
+            throw extractCauseFromExecutionException(e);
+        }
+    }
+
+    public List<TypedMap> getTypedMaps() {
+        try {
+            return Uninterruptibles.getUninterruptibly(getTypeMapsAsync());
+        } catch (ExecutionException e) {
+            throw extractCauseFromExecutionException(e);
+        }
+    }
+
+    public CompletableFuture<Tuple2<TypedMap, ExecutionInfo>> getTypedMapAsyncWithStats() {
+        final RuntimeEngine rte = getRte();
+        final Options options = getOptions();
+
+        final StatementWrapper statementWrapper = getInternalBoundStatementWrapper();
+
+        if (LOGGER.isTraceEnabled()) {
+            LOGGER.trace(format("Execute native query async with execution info : %s",
+                    statementWrapper.getBoundStatement().preparedStatement().getQueryString()));
+        }
+
+        CompletableFuture<ResultSet> cfutureRS = rte.execute(statementWrapper);
+
+        return cfutureRS
+                .thenApply(options::resultSetAsyncListener)
+                .thenApply(statementWrapper::logReturnResults)
+                .thenApply(statementWrapper::logTrace)
+                .thenApply(x -> Tuple2.of(mapRowToTypedMap(x.one()), x.getExecutionInfo()));
+    }
+
+    public CompletableFuture<TypedMap> getTypedMapAsync() {
+        return getTypedMapAsyncWithStats()
+                .thenApply(Tuple2::_1);
+    }
+
+    public Tuple2<TypedMap, ExecutionInfo> getTypedMapWithStats() {
+        try {
+            return Uninterruptibles.getUninterruptibly(getTypedMapAsyncWithStats());
+        } catch (ExecutionException e) {
+            throw extractCauseFromExecutionException(e);
+        }
+    }
+
+    public TypedMap getTypedMap() {
+        try {
+            return Uninterruptibles.getUninterruptibly(getTypedMapAsync());
+        } catch (ExecutionException e) {
+            throw extractCauseFromExecutionException(e);
+        }
+    }
+
+    /***************************************************************************************
+     * Utility API                                                                         *
+     ***************************************************************************************/
     @Override
     public BoundStatement generateAndGetBoundStatement() {
         return getInternalBoundStatementWrapper().getBoundStatement();
