cc3b714480f680e134b7ce1e47e11b0d18a951c3
DuyHai DOAN <doanduyhai@gmail.com>
2019-01-24 14:15:11 +0100

    Change method to find new available ports for Embedded Cassandra

--- achilles-embedded/src/main/java/info/archinnov/achilles/embedded/PortFinder.java
+++ achilles-embedded/src/main/java/info/archinnov/achilles/embedded/PortFinder.java
@@ -18,6 +18,8 @@ package info.archinnov.achilles.embedded;
 
 import java.io.IOException;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.util.stream.IntStream;
 
@@ -31,7 +33,8 @@ public class PortFinder {
 
     public static int findFirstAvailableBetween(int startInclusive, int endExclusive) {
         return IntStream.range(startInclusive, endExclusive)
-            .filter(PortFinder::isAvailable)
+            .filter(port -> PortFinder.isTcpPortAvailable("localhost", port))
+            .filter(port -> PortFinder.isTcpPortAvailable("127.0.0.1", port))
             .findFirst()
             .orElseThrow(() -> new IllegalStateException(
                 "no available port found between " + startInclusive + " and " + endExclusive + "."));
@@ -54,6 +57,7 @@ public class PortFinder {
      * http://stackoverflow.com/questions/434718/sockets-discover-port-
      * availability-using-java
      */
+    @Deprecated
     public static boolean isAvailable(int port) {
         ServerSocket ss = null;
         DatagramSocket ds = null;
@@ -79,6 +83,21 @@ public class PortFinder {
         }
 
         return false;
+    }
+
+    /**
+     * https://stackoverflow.com/questions/434718/sockets-discover-port-availability-using-java
+     */
+    public static boolean isTcpPortAvailable(String hostname, int port) {
+        try (ServerSocket serverSocket = new ServerSocket()) {
+            // setReuseAddress(false) is required only on OSX,
+            // otherwise the code will not work correctly on that platform
+            serverSocket.setReuseAddress(false);
+            serverSocket.bind(new InetSocketAddress(InetAddress.getByName(hostname), port), 1);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
     }
 
     private static int randomBetween(int start, int end) {
